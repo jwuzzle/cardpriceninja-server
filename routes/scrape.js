d5553c0ac287a274dc8e7bf8183f2e913aa9ac3c
Jon Wu <jonathan.wu124@gmail.com>
2024-06-26 10:27:03 -0400

    removed puppeteer.js since scrape.js includes the web scraping code. added in snkrul to the json return. also added waitUntil: networkidle2 to help with javascript page loading of listings on snkrdunk.

--- routes/scrape.js
+++ routes/scrape.js
@@ -72,23 +72,22 @@ router.get("/", async (req, res) => {
 router.post("/used", async (req, res) => {
 
     const { url } = req.body;
+    console.log("page url:", url)
 
     if (!url) {
         return res.status(400).json({ error: "URL is required" });
     }
 
     console.log("Received URL:", url)
+    
 
     try {
         const browser = await puppeteer.launch() //launching the browser and awaiting it because we don't know how long it will take
         const page = await browser.newPage()
-        await page.goto(url) //pull in url from the POST endpoint here
+        await page.goto(url, { waitUntil: 'networkidle2' }); //pull in url from the POST endpoint here
         console.log('Navigated to URL');
 
         const productsHandles = await page.$$("section.product ul.product__block > li.tc-item__used");
-
-
-        let i = 0;
         let productListings = [];
 
 
@@ -96,6 +95,7 @@ router.post("/used", async (req, res) => {
             let price = "Null"
             let evaluation = "Null"
             let image = "Null"
+            let snkrurl = "Null"
 
             try {
                 price = await page.evaluate(el => el.querySelector("a > div.tc-item__detail > p.price").textContent, producthandle);
@@ -112,9 +112,14 @@ router.post("/used", async (req, res) => {
             } catch (error) {
             }
 
+            try {
+                snkrurl = await page.evaluate(el => el.querySelector("a.tc-item__wrapper").getAttribute("href"), producthandle);
+            } catch (error) {
+            }
+
             if (price !== "Null") {
-                console.log(price, evaluation, image)
-                productListings.push({ price, evaluation, image })
+                console.log(price, evaluation, image, snkrurl)
+                productListings.push({ price, evaluation, image, snkrurl })
             }
         }
         await browser.close();
